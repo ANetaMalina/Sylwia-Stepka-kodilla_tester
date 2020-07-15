46e79965c37636ca14da9c12021c3e1f8c6682d8
Sylwia Stepka <sylwiamwst@onet.pl>
2020-07-15 19:09:20 +0200

    init

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Sylwia");
+        System.out.println("28");
+        System.out.println("S");
+    }
+}
+
--- src/main/java/HelloWorld.java
+++
@@ -1,6 +0,0 @@
-public class FirstClass {
-    public static void main(String[] args)
-    {System.out.println("Hello World!");}
-    {System.out.println("Sylwia");}
-    {System.out.println("28");}
-    {System.out.println("S");}
