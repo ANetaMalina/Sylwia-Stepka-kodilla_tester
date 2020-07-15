3f5fcc14afe48930ae3793d0e2ad3fe4e5caa76e
Sylwia Stepka <sylwiamwst@onet.pl>
2020-07-15 19:41:10 +0200

    init

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String imie="Sylwia";
+        int wiek=29;
+        double wzrost=170.5;
+        char inicjal='S';
+        short kasa=50;
+        long ludnosc=8000000000L;
+        System.out.println(imie);
+        System.out.println(wiek);
+        System.out.println(wzrost);
+        System.out.println(inicjal);
+        System.out.println(kasa);
+        System.out.println(ludnosc);
+    }
+}
